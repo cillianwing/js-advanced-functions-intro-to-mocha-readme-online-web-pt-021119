277587e793ee5bd7802f96664495ca8b65beaf9c
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-28 01:21:12 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -17,11 +17,9 @@ describe('Intro to Mocha', function () {
     });
   });
 
-  describe('favoriteIceCream', () => {
-    it('should return the correct sentence when passed an icecream flavor', () => {
-      const result = favoriteIceCream('mint chocolate chip');
-      const expectedResult = 'I love mint chocolate chip';
-      expect(result).toBe(expectedResult);
+  describe('favoriteIceCream()', function () {
+    it('should return the correct sentence when passed an icecream flavor', function () {
+      expect(favoriteIceCream('mint chocolate chip')).toEqual('I love mint chocolate chip');
     });
   });
 
